0732945c069feee1b554ab4db7233d448a00e0c1
Shaw622 <shaw622.dev@gmail.com>
2019-07-22 12:36:56 +0900

    first commit

---
+++ D/paiza.py
@@ -0,0 +1,6 @@
+print('数字を入力')
+a = input("")
+a.split()
+print(a)
+
+
---
+++ D/paiza2.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+print('文字を入力')
+input_line = input()
+list = list(input_line)
+
+a = len(list)
+
+for character in list:
+    if list[] == 'A':
+        list[] = 4
+
+
+    
+    
+
+
+
+print(list)
